c5552e1dfc2df46e48c5a3d7de819be92dc1fc53
DEEPAK A M <deepak.deepu.a.m@gmail.com>
2024-04-23 09:09:43 +0000

    Removed Unwanted Code and Added useTicTacToe hook

--- tic-tac-toe/src/components/TicTacToe.js
+++ tic-tac-toe/src/components/TicTacToe.js
@@ -4,16 +4,24 @@ import useTicTacToe from "../hooks/use-tic-tac-toe";
 export function TicTacToe() {
   const { board, getStatusMessage, handleClick, reset, calculateWinner } =
     useTicTacToe();
+
   return (
     <div className={styles.game}>
       <div className={styles.status}>
-        Player X turn
-        <button className={styles.reset}>Reset</button>
+        {getStatusMessage()}
+        <button className={styles.reset} onClick={reset}>
+          Reset
+        </button>
       </div>
       <div className={styles.board}>
         {board.map((b, idx) => (
-          <button key={idx} className={styles.cell}>
-            X
+          <button
+            key={idx}
+            className={styles.cell}
+            onClick={() => handleClick(idx)}
+            disabled={b !== null}
+          >
+            {b}
           </button>
         ))}
       </div>
--- tic-tac-toe/src/hooks/use-tic-tac-toe.jsx
+++ tic-tac-toe/src/hooks/use-tic-tac-toe.jsx
@@ -6,15 +6,53 @@ const useTicTacToe = () => {
   const [board, setBoard] = useState(initialBoard());
   const [isNextX, setisNextX] = useState(true);
 
-  const WINNING_PATTERS = [];
+  const WINNING_PATTERS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
-  const calculateWinner = () => {};
+  const calculateWinner = (currentBoard) => {
+    for (let i = 0; i < WINNING_PATTERS.length; i++) {
+      const [a, b, c] = WINNING_PATTERS[i];
 
-  const getStatusMessage = () => {};
+      if (
+        currentBoard[a] &&
+        currentBoard[a] === currentBoard[b] &&
+        currentBoard[a] === currentBoard[c]
+      ) {
+        return currentBoard[a];
+      }
+    }
+    return null;
+  };
 
-  const handleClick = () => {};
+  const getStatusMessage = () => {
+    const winner = calculateWinner(board);
+    if (winner) return `Player ${isNextX ? "O" : "X"} win`;
+    if (board.every((b) => b !== null)) return `It's a draw`;
+    return `${isNextX ? "X" : "O"} turn`;
+  };
 
-  const reset = () => {};
+  const handleClick = (index) => {
+    const winner = calculateWinner(board);
+    if (winner || board[index]) return;
+
+    const newBoard = [...board];
+    newBoard[index] = isNextX ? "X" : "O";
+    setBoard(newBoard);
+    setisNextX(!isNextX);
+  };
+
+  const reset = () => {
+    setBoard(initialBoard());
+    setisNextX(true);
+  };
 
   return { board, getStatusMessage, handleClick, reset, calculateWinner };
 };
